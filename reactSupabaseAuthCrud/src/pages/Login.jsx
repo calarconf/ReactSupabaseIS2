519f217ef97308e6693b89fa98c73bffa9905be7
Cristhian Alarcón <82730448+calarconf@users.noreply.github.com>
2024-06-09 21:18:23 -0500

    terminos en pestaña aparte

--- reactSupabaseAuthCrud/src/pages/Login.jsx
+++ reactSupabaseAuthCrud/src/pages/Login.jsx
@@ -54,7 +54,7 @@ function Login() {
 
                   <label className="terms-Link" htmlFor="acceptTerms">
                     <p1>Acepto los </p1>
-                    <Link to="https://seen-ear-f66.notion.site/T-rminos-y-Condiciones-y-Pol-tica-de-Privacidad-en-Compostify-1a8e73ab3b3545b08cab611e81c5b8d2" onClick={() => setTermsAccepted(true)}>
+                    <Link to="https://seen-ear-f66.notion.site/T-rminos-y-Condiciones-y-Pol-tica-de-Privacidad-en-Compostify-1a8e73ab3b3545b08cab611e81c5b8d2" target="_blank" rel="noopener noreferrer" onClick={() => setTermsAccepted(true)}>
                       términos y condiciones
                     </Link>
                     
